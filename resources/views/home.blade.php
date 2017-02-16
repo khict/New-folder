c77426f5b8ebfd4ea07e00b855f368ff2686cd38
Chhan Rathana <chhrathana@gmail.com>
2017-02-16 23:51:51 +0700

    Master layout v1.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'Home Page')
 
 @section('content')
 <div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Admin') }}</title>
+    <title>@yield('title')</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
-
+	<link href="/css/master-layout.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
@@ -21,60 +23,52 @@
     </script>
 </head>
 <body>
-     <div id="app">
-        <!-- Navigation -->
-       <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                       KHICT CMS
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#">Username</a></li>
-                            <li><a href="#">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class = "row">
-        	<div class="col-md-3 left-side">
-		
+     
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="header-menu">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><span>KHICT</span>CMS</a>
+				<ul class="user-menu">
+					<li class="dropdown pull-right">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> User <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
+							<li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Settings</a></li>
+							<li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
+						</ul>
+					</li>
+				</ul>
 			</div>
-	        <div class="col-md-9 right-side">
-				 <div class="row">
-	                <div class="col-lg-12">
-	                    <h1>Coment Management Systems</h1>
-	                </div>
-	                
-	           </div>
-				<div class="row">  
-					@yield('content')  
-	            </div>
-	            
-	        </div>
-        </div>
+							
+		</div><!-- /.container-fluid -->
+	</nav>
+        
+	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
 		
-    </div>
-
+			<ul class="nav menu">
+				<li class="active"><a href=""><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a></li>
+				<li><a href=""><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg> Menu</a></li>
+				<li><a href=""><svg class="glyph stroked line-graph"><use xlink:href="#stroked-line-graph"></use></svg> Category</a></li>
+				<li><a href=""><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Content</a></li>				
+			</ul>
+		</div><!--/.sidebar-->
+		
+		<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+			<div class="row">
+				<ol class="breadcrumb">
+					<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+					<li class="active">@yield('task-name')</li>
+				</ol>
+			</div><!--/.row-->
+			@yield('content')	
+		</div>	<!--/.main-->
+		
     <!-- Scripts -->
     <script src="/js/app.js"></script>
 </body>
